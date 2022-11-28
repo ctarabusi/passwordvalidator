265839857cdde67600acbd18b0048e2fb3b6f24e
mark.nolan <mark.nolan@teufel.de>
2022-11-28 15:19:15 +0100

    refactoring

--- src/main/kotlin/PasswordValidator.kt
+++ src/main/kotlin/PasswordValidator.kt
@@ -1,16 +1,14 @@
 class PasswordValidator {
 
-    fun validate(enteredPassword: String): Boolean {
-
-        return enteredPassword.length > MIN_PASSWORD_LENGTH
+    fun validate(enteredPassword: String) =
+        enteredPassword.length >= MIN_PASSWORD_LENGTH
                 && enteredPassword.any { it.isUpperCase() }
                 && enteredPassword.any { it.isLowerCase() }
                 && enteredPassword.any { it.isDigit() }
                 && enteredPassword.contains("_")
-    }
 
     companion object {
-        const val MIN_PASSWORD_LENGTH = 8
+        const val MIN_PASSWORD_LENGTH = 9
     }
 
 }
